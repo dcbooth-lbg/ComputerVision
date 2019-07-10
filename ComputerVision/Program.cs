d107c55ad25d0c4f0515e85c96b871457e9c4e00
David Booth <david.booth@lloydstsb.co.uk>
2019-07-10 12:27:00 +0100

    Initial upload

--- ComputerVision/Program.cs
+++ ComputerVision/Program.cs
@@ -10,6 +10,10 @@ namespace ComputerVision
     {
         static void Main(string[] args)
         {
+
+            //59330486ed5846a1a32d94506d2ee2b8
+
+            //https://uksouth.api.cognitive.microsoft.com/
         }
     }
 }
